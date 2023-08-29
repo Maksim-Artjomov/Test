9df54549d7cb2a4e85b51e1934939c5e814cd0a0
opilane <opilane@HP-CZC2349HTC>
2023-08-29 16:28:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿public class Student
+{
+    public string Name { get; }
+    public int Age { get; }
+    private readonly string _city;
+
+    public Student(string name, int age, string city)
+    {
+        Name = name;
+        Age = age;
+        _city = city;
+    }
+
+    public string GetCity()
+    {
+        return _city;
+    }
+}
+
+public class StudentMain
+{
+    public static void Main()
+    {
+        Student student = new Student("John", 17, "London");
+
+        Console.WriteLine(student.Name);
+        Console.WriteLine(student.Age);
+        Console.WriteLine(student.GetCity());
+        Console.ReadLine();
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test
+{
+    internal class Student
+    {
+        public string Name { get; }
+        public int age;
+        private readonly string _city;
+        public string status;
+        public int birthday;
+
+        public Student(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            _city = city;
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                if (age < 18) status = "alaealine";
+                else status = "täiskasvanud";
+            }
+        }
+        public string Status
+        {
+            get { return status; }
+        }
+
+        public string GetCity()
+        {
+            return _city;
+        }
+        public int Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                age= value;
+                birthday = ;
+            }
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(GetCity());
+        }
+    }
+}
